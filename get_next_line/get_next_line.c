c0c19697e911589a1a219a474d61a95090ecf46d
elhousseine lakhfif <elakhfif@c2r8p6.1337.ma>
2023-11-22 04:14:35 +0100

    functions optimized

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 03:26:47 by elakhfif          #+#    #+#             */
-/*   Updated: 2023/11/22 03:26:49 by elakhfif         ###   ########.fr       */
+/*   Updated: 2023/11/22 04:14:22 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,39 @@
 # define BUFFER_SIZE 32
 #endif
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    int i = 0;
-    int rd = 0;
-    char c;
+	char	*buffer;
+	int		rd;
+	int		i;
+	char	c;
 
-    if (BUFFER_SIZE <= 0)
-        return (NULL);
-    char *buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    while ((rd = read(fd, &c, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0)
-    {
-        buffer[i++] = c;
-        if (c == '\n')
-            break;
-    }
-    buffer[i] = '\0';
-    if (rd == -1 || i == 0 || (!buffer[i - 1] && !rd))
-        return (free(buffer), NULL);
-    return (buffer);
+	i = 0;
+	rd = 0;
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while ((rd = read(fd, &c, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0)
+	{
+		buffer[i++] = c;
+		if (c == '\n')
+			break;
+	}
+	buffer[i] = '\0';
+	if (rd == -1 || i == 0 || (!buffer[i - 1] && !rd))
+		return (free(buffer), NULL);
+	return (buffer);
 }
 
 #include <stdio.h>
-
-int	main()
+int main()
 {
 	int fd = open("test.txt", O_RDONLY);
-	char *line = NULL;
+	char *line;
 	while ((line = get_next_line(fd)))
 	{
-		printf("%s", line);
+		printf("%s\n", line);
 		free(line);
 	}
-	close(fd);
 	return (0);
 }
